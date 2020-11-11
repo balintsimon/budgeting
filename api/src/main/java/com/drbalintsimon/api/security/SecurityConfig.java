f077b1d1d7e785d6cdaa5f603722727ca589eac9
dr. Simon Bálint <balladin0@gmail.com>
2020-11-11 20:46:15 +0100

    Add basic java security services for testing.

---
+++ api/src/main/java/com/drbalintsimon/api/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.drbalintsimon.api.security;
+
+import com.drbalintsimon.api.security.service.JwtTokenServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@CrossOrigin
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+//                .anyRequest().denyAll()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowCredentials(true);
+        // configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-*", "Origin", "Content-type", "Accept", "Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        // configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ api/src/main/java/com/drbalintsimon/api/security/service/JwtTokenServices.java
@@ -0,0 +1,110 @@
+package com.drbalintsimon.api.security.service;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class JwtTokenServices {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "secret";
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 36000000; // 10 hours
+
+    private final String rolesFieldName = "roles";
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    /**
+     * Create a token.
+     * @param username
+     * @param roles
+     * @return a JWT
+     */
+    public String createToken(String username, List<String> roles) {
+        // Add a custom field to the token
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(rolesFieldName, roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    /**
+     * Filter and return cookie from incoming request.
+     * @param request
+     * @return cookie if present
+     */
+    public Optional<Cookie> getTokenCookieFromRequest(HttpServletRequest request) {
+        if (request.getCookies() == null) return Optional.empty();
+        System.out.println("Cookies: " + request.getCookies().toString());
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> "JWT".equals(cookie.getName()))
+                .findFirst();
+    }
+
+    /**
+     * Returns token from the incoming request if present. Used as a wrapper for any authentication method
+     * (token method, local storage, etc), so code change would remain inside this class.
+     * @param request
+     * @return
+     */
+    public String getTokenFromRequest(HttpServletRequest request) {
+        return getTokenCookieFromRequest(request).map(Cookie::getValue).orElse(null);
+    }
+
+    /**
+     * Checks token validity, including expiration.
+     * @param token
+     * @return true if token is valid, otherwise false
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return claims.getBody().getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            log.debug("JWT token invalid " + e);
+        }
+        return false;
+    }
+
+    /**
+     * Parses the username and roles from the token. Since the token is signed we can be sure its valid information.
+     * Note that it does not make a DB call to be super fast!
+     * This could result in returning false data (e.g. the user was deleted, but their token has not expired yet)
+     * To prevent errors because of this make sure to check the user in the database for more important calls!
+     */
+    public Authentication parseUserFromTokenInfo(String token) throws UsernameNotFoundException {
+        Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        String username = body.getSubject();
+        List<String> roles = (List<String>) body.get(rolesFieldName);
+        List<SimpleGrantedAuthority> authorities = roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toCollection(LinkedList::new));
+        return new UsernamePasswordAuthenticationToken(username, "", authorities);
+    }
+}
