839f0c3fcee7a16e1e6d59a8b334ef8614c8d0af
dr. Simon Bálint <balladin0@gmail.com>
2020-11-12 21:50:52 +0100

    Rework save visitor endpoint
    Since visitor registration changed, endpoint reflects the corresponding return value and logic.

--- api/src/main/java/com/drbalintsimon/api/controller/VisitorController.java
+++ api/src/main/java/com/drbalintsimon/api/controller/VisitorController.java
@@ -1,12 +1,15 @@
 package com.drbalintsimon.api.controller;
 
 import com.drbalintsimon.api.model.Visitor;
-import com.drbalintsimon.api.repository.UserRepository;
-import com.drbalintsimon.api.serice.dao.VisitorDAO;
+import com.drbalintsimon.api.model.dto.VisitorCredentialsDTO;
+import com.drbalintsimon.api.service.VisitorService;
+import com.drbalintsimon.api.service.dao.VisitorDAO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,20 +20,17 @@ import java.util.List;
 @Slf4j
 public class VisitorController {
     private final VisitorDAO visitorDAO;
-    private final UserRepository repository;
+    private final VisitorService visitorService;
 
     @GetMapping("/user")
     public List<Visitor> getAllUsers() {
-        System.out.println("Get all users");
+        log.info("Get all users");
         return visitorDAO.findAll();
     }
 
     @PostMapping("/user")
-    public void saveNewUser() {
-        System.out.println("Add new user");
-//        BudgetingUser newBudgetingUser = BudgetingUser.builder().id(1L).name("BB King").password("whatnot").email("new@new.hu").build();
-        Visitor newVisitor = Visitor.builder().name("BB King").password("whatnot").email("new@new.hu").build();
-        System.out.println(newVisitor);
-        repository.save(newVisitor);
+    public ResponseEntity saveNewUser(@RequestBody VisitorCredentialsDTO credentialsDTO) {
+        log.info("Add new user");
+        return visitorService.tryRegister(credentialsDTO);
     }
 }
